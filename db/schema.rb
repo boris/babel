9ff1a2ce1ba138766a08c381809a9329fdabc70e
Boris Quiroz <boris@insert-coin.org>
2015-09-13 22:22:33 -0300

    user auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914002641) do
+ActiveRecord::Schema.define(version: 20150914005148) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20150914002641) do
     t.string   "editor"
     t.string   "year"
     t.string   "read"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
