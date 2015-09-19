cf75a8e94f680939197b4029fb3dd7976c6b3449
Boris Quiroz <boris@insert-coin.org>
2015-09-18 23:38:41 -0300

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :pty, true
 set :ssh_options, { :forward_agent => true }
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/unicorn.rb', 'db/babel_prod')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
