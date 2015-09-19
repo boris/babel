11b9954c2d9477d12cc85876bbc51defb7fb3242
Boris Quiroz <boris@insert-coin.org>
2015-09-19 01:26:45 -0300

    fix roles

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 namespace :deploy do
 
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    on roles(:all), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
