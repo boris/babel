76b318c639a8219d996b8cef6eeeadd7ea8fb5bf
Boris Quiroz <boris@insert-coin.org>
2015-11-08 21:36:29 -0300

    add methods for country_{code, name}

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,14 @@
 class Book < ActiveRecord::Base
   validates :title, :author, presence: true,
     length: { minimum: 1 }
+
+  def country_code
+    country_code = country.to_s
+  end
+
+  def country_name
+    country = ISO3166::Country[country_code]
+    country.translations[I18n.locale.to_s] || country.name
+  end
+
 end
