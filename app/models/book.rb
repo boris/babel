95f13212e705437e1bf54c003bc5694372324b1c
Boris Quiroz <boris@insert-coin.org>
2017-05-07 01:11:52 -0300

    do not translate countries

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,7 +8,7 @@ class Book < ActiveRecord::Base
 
   def country_name
     country = ISO3166::Country[country_code]
-    country.translations[I18n.locale.to_s] || country.name
+#    country.translations[I18n.locale.to_s] || country.name
   end
 
 end
