4456148ed6d664d66aebe8eb3cea602a5de8b624
Boris Quiroz <boris@insert-coin.org>
2017-08-07 22:27:00 -0400

    schema migrations

--- db/schema.rb
+++ db/schema.rb
@@ -26,13 +26,6 @@ ActiveRecord::Schema.define(version: 20150920031932) do
     t.text     "borrowed_to", limit: 65535
   end
 
-  create_table "borrows", force: :cascade do |t|
-    t.boolean  "borrowed"
-    t.text     "who",        limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
     t.string   "email",           limit: 255
