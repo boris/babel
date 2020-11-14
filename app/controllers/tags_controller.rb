a101f3ab76d23fc3c69714f2786209d52c157170
Boris Quiroz <boris@insert-coin.org>
2020-11-14 20:12:21 -0300

    Tags controller

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,12 @@
+class TagsController < ApplicationController
+  def index
+    @tags = ActsAsTaggableOn::Tag.all
+  end
+
+  def show
+    @tag =  ActsAsTaggableOn::Tag.find(params[:id])
+    @book = Book.tagged_with(@tag.name)
+  end
+end
+
+
