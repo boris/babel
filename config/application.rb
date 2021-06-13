75e61103473f6660b0a2b4ea51e2681125894d7e
Boris Quiroz <boris@insert-coin.org>
2021-06-12 21:32:39 -0400

    remove deprecated

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module Library
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
   end
 end
